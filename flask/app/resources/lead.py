59c214af524b3c051e11fc55f92f198a9463f228
Lamby <lambertotamayo15@gmail.com>
2023-02-23 17:02:08 +0800

    update lead resource

--- flask/app/resources/lead.py
+++ flask/app/resources/lead.py
@@ -198,10 +198,11 @@ class LeadResultResource(Resource):
             return {"id": result_id, "message": f"status {ch_code} created."}, 201
 
         except Exception as ex:
-            if connection:
-                bank_db.put_conn(connection)
-
-            if main_db_connection:
-                main_db.put_conn(main_db_connection)
+            if 'connection' in locals():
+                if connection:
+                    bank_db.put_conn(connection)
+            if 'main_db_connection' in locals():
+                if main_db_connection:
+                    main_db.put_conn(main_db_connection)
             print(ex)
             return {"error": f"{ex}"}, 400
